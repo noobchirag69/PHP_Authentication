27f67e7d4c50574f44140aa2a176fbe37ffbe801
Chirag Chakraborty <chiragchakraborty48@gmail.com>
2023-11-09 13:44:55 +0530

    Project Name

--- partials/navbar.php
+++ partials/navbar.php
@@ -1,5 +1,5 @@
 <div class="navbar" tabindex="0">
-    <h1 tabindex="0">PHP Dashboard</h1>
+    <h1 tabindex="0">PHP Auth</h1>
     <a title="Source Code" href="https://github.com/noobchirag69/PHP_Authentication" target="_blank">
         <i class="fa-2x fa-brands fa-github"></i>
     </a>
