0df683cbf8b135fc08669bb9012306f38766b6c8
Chirag Chakraborty <chiragchakraborty48@gmail.com>
2023-11-09 15:07:29 +0530

    Delete Page

--- delete-account.php
+++ delete-account.php
@@ -16,9 +16,9 @@ if (!isset($_SESSION['id']) && !isset($_SESSION['username']) && !isset($_SESSION
 	<div tabindex="0" class="wrapper">
 		<h1 tabindex="0">Delete Account</h1>
 		<form action="delete-account-check.php" method="POST">
-            <label for="password">If you're sure you want to delete your account, please type your password to confirm deletion.</label>
+			<label tabindex="0" for="password">If you're sure you want to delete your account, please type your password to confirm deletion.</label>
 			<input type="password" id="password" placeholder="Password" name="password" required />
-			<button type="submit" name="delete">Delete <i class="fa-solid fa-trash"></i></button>
+			<button id="delete" type="submit" name="delete">Delete <i class="fa-solid fa-skull-crossbones"></i></button>
 		</form>
 		<a class="option" href="dashboard.php"><i class="fa-solid fa-left-long"></i> Back to Dashboard</a>
 	</div>
